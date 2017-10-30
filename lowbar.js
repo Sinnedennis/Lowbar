4e1103be60752836158c86c088848169a4c162ab
Dennis Foster <contact@DennisFoster.co.uk>
2017-10-30 21:27:57 +0000

    Adding #first

--- lowbar.js
+++ lowbar.js
@@ -9,4 +9,9 @@ _.values = function (object) {
   return Object.values(object);
 };
 
+_.first = function (array, n) {
+  if (!Array.isArray(array)) return null;
+  return n === undefined ? array[0] : array.slice(0, n);
+};
+
 module.exports = _;
